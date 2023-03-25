4e91e9140167a45b7e784385c9a1b093ae52ded0
Saad <saadsohailbajwa@gmail.com>
2023-03-26 03:31:53 +0500

    modified line 5

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ print("this was before hello world but then changed in the desktop-python-edit b
 print("hello new-file")
 print("this is branch 3")
 print("added in the laptop-branch-1")
-print("this is print from laptop")
+print("refers to last statment of this line","this is print from laptop","another laptop-branch-1 change")
 print("this is print from desktop after getting pull from laptop")
